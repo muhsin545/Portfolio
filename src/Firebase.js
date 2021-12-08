0652597fa25db266d93d4b950e2538cb313d16c5
muhsin545 <muhibullahmuhsin545@gmail.com>
2021-12-08 20:38:07 +0600

    compleated almost

---
+++ src/Firebase.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC4MLiWw80FY0zxz19hIEHJIfHyLlZDGo4",
+    authDomain: "portfolio-9bab2.firebaseapp.com",
+    projectId: "portfolio-9bab2",
+    storageBucket: "portfolio-9bab2.appspot.com",
+    messagingSenderId: "619496274367",
+    appId: "1:619496274367:web:e2ab2b23570ebd86cb490a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
